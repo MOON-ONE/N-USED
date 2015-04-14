889db07b028b5e8979d9db1fac6168f21cbda602
Wang Jinghan <phantom.there@gmail.com>
2015-04-15 05:47:50 +0800

    data

---
+++ static/javascripts/data.js
@@ -0,0 +1,157 @@
+var books = [
+{
+    'moduleCode': 'CS3241',
+    'title': 'title 1',
+    'edition': 'edition 1',
+    'authors': 'author 1',
+    'condition': {
+        'rank': 10,
+        'checkList': [true, false, false, false, true, false]
+    },
+    'description': "Description 1",
+    'price': 20,
+    'sellId': "Uskdhiu63f"
+},
+{
+    'moduleCode': 'CS3242',
+    'title': 'title 2',
+    'edition': 'edition 1',
+    'authors': 'author 1',
+    'condition': {
+        'rank': 10,
+        'checkList': [true, false, false, false, true, false]
+    },
+    'description': "Description 1",
+    'price': 20,
+    'sellId': "Uskdhiu63f"
+},
+{
+    'moduleCode': 'CS3243',
+    'title': 'title 3',
+    'edition': 'edition 1',
+    'authors': 'author 1',
+    'condition': {
+        'rank': 10,
+        'checkList': [true, false, false, false, true, false]
+    },
+    'description': "Description 1",
+    'price': 20,
+    'sellId': "Uskdhiu63f"
+},
+{
+    'moduleCode': 'CS3244',
+    'title': 'title 4',
+    'edition': 'edition 1',
+    'authors': 'author 1',
+    'condition': {
+        'rank': 10,
+        'checkList': [true, false, false, false, true, false]
+    },
+    'description': "Description 1",
+    'price': 20,
+    'sellId': "Uskdhiu63f"
+},
+{
+    'moduleCode': 'CS3241',
+    'title': 'title 1',
+    'edition': 'edition 1',
+    'authors': 'author 1',
+    'condition': {
+        'rank': 10,
+        'checkList': [true, false, false, false, true, false]
+    },
+    'description': "Description 1",
+    'price': 20,
+    'sellId': "Uskdhiu63f"
+},
+{
+    'moduleCode': 'CS3242',
+    'title': 'title 2',
+    'edition': 'edition 1',
+    'authors': 'author 1',
+    'condition': {
+        'rank': 10,
+        'checkList': [true, false, false, false, true, false]
+    },
+    'description': "Description 1",
+    'price': 20,
+    'sellId': "Uskdhiu63f"
+},
+{
+    'moduleCode': 'CS3243',
+    'title': 'title 3',
+    'edition': 'edition 1',
+    'authors': 'author 1',
+    'condition': {
+        'rank': 10,
+        'checkList': [true, false, false, false, true, false]
+    },
+    'description': "Description 1",
+    'price': 20,
+    'sellId': "Uskdhiu63f"
+},
+{
+    'moduleCode': 'CS3244',
+    'title': 'title 4',
+    'edition': 'edition 1',
+    'authors': 'author 1',
+    'condition': {
+        'rank': 10,
+        'checkList': [true, false, false, false, true, false]
+    },
+    'description': "Description 1",
+    'price': 20,
+    'sellId': "Uskdhiu63f"
+},
+{
+    'moduleCode': 'CS3241',
+    'title': 'title 1',
+    'edition': 'edition 1',
+    'authors': 'author 1',
+    'condition': {
+        'rank': 10,
+        'checkList': [true, false, false, false, true, false]
+    },
+    'description': "Description 1",
+    'price': 20,
+    'sellId': "Uskdhiu63f"
+},
+{
+    'moduleCode': 'CS3242',
+    'title': 'title 2',
+    'edition': 'edition 1',
+    'authors': 'author 1',
+    'condition': {
+        'rank': 10,
+        'checkList': [true, false, false, false, true, false]
+    },
+    'description': "Description 1",
+    'price': 20,
+    'sellId': "Uskdhiu63f"
+},
+{
+    'moduleCode': 'CS3243',
+    'title': 'title 3',
+    'edition': 'edition 1',
+    'authors': 'author 1',
+    'condition': {
+        'rank': 10,
+        'checkList': [true, false, false, false, true, false]
+    },
+    'description': "Description 1",
+    'price': 20,
+    'sellId': "Uskdhiu63f"
+},
+{
+    'moduleCode': 'CS3244',
+    'title': 'title 4',
+    'edition': 'edition 1',
+    'authors': 'author 1',
+    'condition': {
+        'rank': 10,
+        'checkList': [true, false, false, false, true, false]
+    },
+    'description': "Description 1",
+    'price': 20,
+    'sellId': "Uskdhiu63f"
+}];
