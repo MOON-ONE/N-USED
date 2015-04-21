0941bd5fbb293d1b3606078452409f70fda8e2bb
Lei Mingyu <uygnim.iel@gmail.com>
2015-04-21 18:55:33 +0800

    refactor

--- static/javascripts/custom.js
+++ static/javascripts/custom.js
@@ -8,75 +8,24 @@ function viewDidLoad() {
 
 	$('[data-toggle="tooltip"]').tooltip();
 
-	// 总完
-
-
-	// 王狗
-
-	$("#n-home .sort-button").click(function() {
-		var moreOptionBar = $("#n-home .n-more-option-bar");
-		if (moreOptionBar.css("display") == "none") {
-			moreOptionBar.show();
-			$(this).html('<i class="fa fa-chevron-up">');
-			updateTableContainerHeight(235 + moreOptionBar.height());
-		} else {
-			moreOptionBar.hide();
-			$(this).html('<i class="fa fa-chevron-down">');
-			updateTableContainerHeight(70);
-		}
-	});
-
-	$("#selected-only-button").click(function() {
-		
-		if ($(this).hasClass("highlight-button")) {
-			$(this).removeClass("highlight-button");
-			angular.element("body").scope().search.isSelected = ""
-		} else {
-			$(this).addClass("highlight-button");
-			angular.element("body").scope().search.isSelected = true	
-		}
-
-		angular.element("body").scope().$apply();
-		
-	})
-
 	var table = $(".n-book-list-table table");
 	table.floatThead({
 		scrollContainer: function($table){
 			return $('.n-book-list-table');
 		}
 	});
-	// 王狗完
 
-	// 林狗
 	$(".fancybox").fancybox();
-
-	// 林狗完
-
-
-	// 孙狗
-	$(document).on("click", "#n-account .content h4 span", function() {
-		$("#n-account .content h4 span").removeClass("selected");
-		$(this).addClass("selected")
-		var showContentID = $(this).attr("href");
-		$("#n-account .content .content-block").css("display", "none");
-		$("#n-account .content .content-block" + showContentID).css("display", "block");
-	})
-
 
 	$("#n-account tr").each(function(index) {
 		if ($(this).attr("is-sold") == "true") {
 			$(this).addClass("sold-entry-row");
 			$(this).children().addClass("sold-entry-row");
 		}
-	})
-
-	// 孙狗完
-
+	});
 }
 
-// 雷狗
-
+// Nav
 $(document).on("click", "div[id^='n-nav-']", function() {
 	if ($(this).attr('id') == 'n-nav-logout') {
 		// logout
@@ -99,7 +48,6 @@ $(document).on("click", "div[id^='n-nav-']", function() {
 	}
 });
 
-
 var textTimeout = 200;
 
 $(document).on("click", "#n-hamburger-icon", function() {
@@ -111,13 +59,10 @@ $(document).on("click", "#n-hamburger-icon", function() {
 	textTimeout = 200 - textTimeout;
 	return false;
 });
+// Nav End
 
 
-
-// 雷狗完
-
-
-//Home
+// Home
 function updateTableContainerHeight(height) {
 	var windowHeight = $(window).height();
 	var tableContainer = $(".n-book-list-table").css("height", (windowHeight - height) + "px");
@@ -144,6 +89,32 @@ function updateGrandCheckbox() {
 	}
 }
 
+$(document).on("click", "#n-home .sort-button", function() {
+	var moreOptionBar = $("#n-home .n-more-option-bar");
+	if (moreOptionBar.css("display") == "none") {
+		moreOptionBar.show();
+		$(this).html('<i class="fa fa-chevron-up">');
+		updateTableContainerHeight(235 + moreOptionBar.height());
+	} else {
+		moreOptionBar.hide();
+		$(this).html('<i class="fa fa-chevron-down">');
+		updateTableContainerHeight(70);
+	}
+});
+
+$(document).on("click", "#selected-only-button", function() {
+	
+	if ($(this).hasClass("highlight-button")) {
+		$(this).removeClass("highlight-button");
+		angular.element("body").scope().search.isSelected = ""
+	} else {
+		$(this).addClass("highlight-button");
+		angular.element("body").scope().search.isSelected = true	
+	}
+
+	angular.element("body").scope().$apply();
+});
+
 $(document).on("click", "#n-home .grand-checkbox", function() {
 	$("#n-home .col-selection input").each(function(index) {
 		if ($(this).prop('checked') != $("#n-home .grand-checkbox").prop('checked')) {
@@ -161,7 +132,7 @@ $(document).on("click", "#n-home .grand-checkbox", function() {
 	});
 	updateBookList();
 	console.log(books);
-})
+});
 
 $(document).on("click", ".book-item .check", function() {
 	var checkbox = $(this).find("input");
@@ -195,14 +166,6 @@ $(document).on("click", "#n-home .col-selection input", function() {
 //End of Home
 
 //Account
-
-$(document).on("click", "#n-account .stars .btn-danger", function() {
-	var res = confirm("This action is not recoverable! Do you still want to continue?");
-	if (res == true) {
-		removeFavorites(getSelectedFavorite());
-	}
-});
-
 function getSelectedFavorite() {
 	var selectedItems = []
 	$("#n-account .stars .acc-col-selection input").each(function(index) {
@@ -216,6 +179,21 @@ function getSelectedFavorite() {
 	})
 	return selectedItems;
 }
+
+$(document).on("click", "#n-account .content h4 span", function() {
+	$("#n-account .content h4 span").removeClass("selected");
+	$(this).addClass("selected")
+	var showContentID = $(this).attr("href");
+	$("#n-account .content .content-block").css("display", "none");
+	$("#n-account .content .content-block" + showContentID).css("display", "block");
+});
+
+$(document).on("click", "#n-account .stars .btn-danger", function() {
+	var res = confirm("This action is not recoverable! Do you still want to continue?");
+	if (res == true) {
+		removeFavorites(getSelectedFavorite());
+	}
+});
 
 
 //End of Account
