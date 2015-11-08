6238223ddf0c2ba2f8c37b0a7d0b4b3b60a73dde
Lei Mingyu <uygnim.iel@gmail.com>
2015-11-08 23:36:36 +0800

    remove jinghan

--- jinghan.sh
+++ jinghan.sh
@@ -1,19 +1,27 @@
-#!/bin/sh
+# #!/bin/sh
 
-git filter-branch --env-filter '
+# git filter-branch --env-filter '
 
-OLD_EMAIL="phantom.there@gmail.com"
-CORRECT_NAME="Jinghan Wang"
-CORRECT_EMAIL="jinghan.w@outlook.com"
+# OLD_EMAIL="phantom.there@gmail.com"
+# CORRECT_NAME="Jinghan Wang"
+# CORRECT_EMAIL="jinghan.w@outlook.com"
 
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+# if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+# then
+#     export GIT_COMMITTER_NAME="$CORRECT_NAME"
+#     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+# fi
+# if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+# then
+#     export GIT_AUTHOR_NAME="$CORRECT_NAME"
+#     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+# fi
+# ' --tag-name-filter cat -- --branches --tags
+
+git filter-branch --commit-filter '
+if [ "$GIT_AUTHOR_NAME" = "null09264" ];
 then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
-    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-fi
-' --tag-name-filter cat -- --branches --tags
+        skip_commit "$@";
+else
+        git commit-tree "$@";
+fi' HEAD
